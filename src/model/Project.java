054f8f8a859d81d6099faf0f7e9019bbea5640c8
Haris Ali <malikharis2003@gmail.com>
2024-11-19 00:09:37 +0500

    Initial commit

---
+++ src/model/Project.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author samtheradiant
+ */
+public class Project {
+    
+    // ATTRIBUTES
+    
+    // TODO: Decide whether to add projectID as attribute
+    // TODO: Decide whether to add assignedTo as attribute
+    private String title, description;
+    private ArrayList<Task> tasks;
+    private Status projectStatus;
+    
+    // METHODS
+    
+    
+}
