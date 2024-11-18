033437b30fe342dd8a5aa68c1eb0dd33a69c507c
i c <i c@DESKTOP-AE3FPU7>
2024-11-18 21:44:15 +0500

    Removing trash

--- src/gui/TestLogin.java
+++ src/gui/TestLogin.java
@@ -2,12 +2,7 @@
 package gui;
 
 public class TestLogin {
-    int test;
-
-    @Override
-    public String toString() {
-        return "TestLogin{" + "test=" + test + '}';
-    }
+    
     public TestLogin() {
     }
     
