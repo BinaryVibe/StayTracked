ff8f8a0713cb23c41709a3716892b7dd14078f4e
i c <i c@DESKTOP-AE3FPU7>
2024-11-18 21:41:46 +0500

    Override toString()

--- src/gui/TestLogin.java
+++ src/gui/TestLogin.java
@@ -2,7 +2,12 @@
 package gui;
 
 public class TestLogin {
+    int test;
 
+    @Override
+    public String toString() {
+        return "TestLogin{" + "test=" + test + '}';
+    }
     public TestLogin() {
     }
     
