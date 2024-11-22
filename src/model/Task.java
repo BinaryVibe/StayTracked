c6f3ac9be719e13126d9936098995b77690e56e3
Haris Ali <malikharis2003@gmail.com>
2024-11-23 01:19:56 +0500

    Initial commit

--- src/model/Task.java
+++ src/model/Task.java
@@ -4,10 +4,82 @@
  */
 package model;
 
+import java.util.Date;
+
 /**
  *
  * @author samtheradiant
  */
 public class Task {
-    
+
+    private String title, description;
+    private Status taskStatus;
+    private Date startDate, endDate;
+    private Priority taskPriority;
+
+    public Task() {
+        this.title = "Untitled";
+        this.description = "";
+        this.startDate = new Date();
+        this.taskStatus = Status.PENDING;
+        this.taskPriority = Priority.NONE;
+    }
+
+    public Task(String title, String description, Status taskStatus, Date startDate, Date endDate, Priority taskPriority) {
+        this.title = title;
+        this.description = description;
+        this.taskStatus = taskStatus;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.taskPriority = taskPriority;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Status getTaskStatus() {
+        return taskStatus;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public Priority getTaskPriority() {
+        return taskPriority;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setTaskStatus(Status taskStatus) {
+        this.taskStatus = taskStatus;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public void setTaskPriority(Priority taskPriority) {
+        this.taskPriority = taskPriority;
+    }
+
 }
