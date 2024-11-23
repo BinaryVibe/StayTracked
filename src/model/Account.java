1356608500c7fa252321656f14755527cc8a583d
Ersatz-xD <aayan.shazim@gmail.com>
2024-11-23 15:23:50 +0500

    Skeleton of Account Class

---
+++ src/model/Account.java
@@ -0,0 +1,17 @@
+
+package model;
+
+public abstract class Account {
+    protected String userName, email, password;
+    
+    protected void logIn(){
+        
+    }
+    protected void logOut(){
+        
+    }
+    protected void resetPass(){
+        
+    }
+    protected abstract void viewDetails();
+}
