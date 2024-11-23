0cecac010c70354ea41bb7138b5fd898326886ca
Ersatz-xD <aayan.shazim@gmail.com>
2024-11-23 22:12:58 +0500

    initial commit for Team Class

---
+++ src/model/Team.java
@@ -0,0 +1,27 @@
+
+package model;
+
+import java.util.ArrayList;
+
+public class Team {
+    private String teamName, teamID;
+    private ManagerAccount manager;
+    ArrayList<NormalAccount> teamMembers = new ArrayList<>();
+    
+
+   
+    public void viewDetails() {
+     //View Details of Team Account.   
+    }
+    
+    public void addTeamMember(NormalAccount member){
+        //add a member
+    }
+    public void removeTeamMember(NormalAccount member){
+        //remove members
+    }
+    public void showTeamMembers(){
+        // shows a list of All existing team members
+    }
+    
+}
