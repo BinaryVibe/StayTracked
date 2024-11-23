1c29aeb56bcb8640501984a192c23881c7612e0f
Ersatz-xD <aayan.shazim@gmail.com>
2024-11-23 21:42:30 +0500

    Initial Commit for NormalAccount Class.

---
+++ src/model/NormalAccount.java
@@ -0,0 +1,16 @@
+
+package model;
+
+public class NormalAccount extends Account {
+    
+    private String userName, contactNum;
+    private boolean isPartOfTeam;
+    
+    @Override
+    protected void viewDetails() {
+       //Display Personal Account details
+    }
+    
+    
+    
+}
