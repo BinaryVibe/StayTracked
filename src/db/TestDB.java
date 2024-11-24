7c261a0357ca6d8e1a9d9434521a633071b05cdd
Ersatz-xD <aayan.shazim@gmail.com>
2024-11-24 22:36:56 +0500

    initial commit for testing class
    this class just to test connectivity with database

---
+++ src/db/TestDB.java
@@ -0,0 +1,16 @@
+
+package db;
+
+import java.sql.Connection;
+
+public class TestDB {
+    public static void main(String[] args) {
+		
+		Connection c = DBConnectionManager.getConnection();
+		
+		if(c != null) {
+			System.out.println("Connection Successful");
+		}
+		
+	}
+}
