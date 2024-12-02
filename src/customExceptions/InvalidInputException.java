0cc2c22ab274dec4618f6d2e5e8e43620c375a6c
Ersatz-xD <aayan.shazim@gmail.com>
2024-12-02 22:29:15 +0500

    deleted checkInput method

--- src/customExceptions/InvalidInputException.java
+++ src/customExceptions/InvalidInputException.java
@@ -1,15 +1,8 @@
-
 package customExceptions;
 
 public class InvalidInputException extends Exception {
-    public InvalidInputException(String message){
+
+    public InvalidInputException(String message) {
         super(message);
-    }
-    
-     public static void checkInput(String email, String password) throws InvalidInputException {
-         
-       /* if () {
-            throw new InvalidInputException(" Incorrect email OR passwrod! ");
-        }*/
     }
 }
