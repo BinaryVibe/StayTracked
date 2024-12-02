c797f69d4a0376c51cd6907ae0c45e17f0d955c6
Ersatz-xD <aayan.shazim@gmail.com>
2024-12-02 23:18:25 +0500

    constructor of Normal Account Class

--- src/model/NormalAccount.java
+++ src/model/NormalAccount.java
@@ -3,6 +3,13 @@ package model;
 
 public class NormalAccount extends Account {
     private boolean isPartOfTeam;
+
+    public NormalAccount(boolean isPartOfTeam, String firstName, String lastName, String userName, String contactNum, String email, String password) {
+        super(firstName, lastName, userName, contactNum, email, password);
+        this.isPartOfTeam = isPartOfTeam;
+    }
+    
+    
     
     @Override
     public void viewDetails() {
