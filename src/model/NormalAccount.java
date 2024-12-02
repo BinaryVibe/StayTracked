4ddcd64c28041d0433f28a52ea007d72dc31ed8f
Ersatz-xD <aayan.shazim@gmail.com>
2024-12-02 23:19:34 +0500

    formatted

--- src/model/NormalAccount.java
+++ src/model/NormalAccount.java
@@ -1,7 +1,7 @@
-
 package model;
 
 public class NormalAccount extends Account {
+
     private boolean isPartOfTeam;
 
     public NormalAccount(boolean isPartOfTeam, String firstName, String lastName, String userName, String contactNum, String email, String password) {
@@ -16,13 +16,10 @@ public class NormalAccount extends Account {
     public boolean isIsPartOfTeam() {
         return isPartOfTeam;
     }
-    
-    
+
     @Override
     public void viewDetails() {
-       //Display Personal Account details
+        //Display Personal Account details
     }
-    
-    
-    
+
 }
