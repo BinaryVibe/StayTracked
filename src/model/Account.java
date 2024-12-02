0ac9b204776d20d3fee69fa493cfbe305eec3284
Ersatz-xD <aayan.shazim@gmail.com>
2024-12-02 23:14:27 +0500

    setters of Account Class

--- src/model/Account.java
+++ src/model/Account.java
@@ -13,6 +13,37 @@ public abstract class Account {
         this.email = email;
         this.password = password;
     }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setContactNum(String contactNum) {
+        this.contactNum = contactNum;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setAccountID(int accountID) {
+        this.accountID = accountID;
+    }
+    
+    
+    
     
     protected void logIn(){
         
