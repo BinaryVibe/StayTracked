0d9ff3c0c15c2c636fc60dedfd61331c917246e5
Haris Ali <malikharis2003@gmail.com>
2024-12-08 01:21:19 +0500

    Initial commit

---
+++ src/db/ProjectsDB.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package db;
+
+/**
+ *
+ * @author samtheradiant
+ */
+public class ProjectsDB {
+    // Add method to store project with respect to accountID
+}
