e4e69163afe4f2a1a860072f045658a3ef2c6af2
Haris Ali <malikharis2003@gmail.com>
2024-12-08 01:35:12 +0500

    Initial commit

---
+++ src/model/DataLayer.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ * DataLayer is the class that is responsible for 
+ * transferring data between the GUI and database classes
+ *
+ * @author samtheradiant
+ */
+public class DataLayer {
+    
+    
+    public static void saveProjecttoDB(Project project) {
+        // Add code
+    }  
+    
+}
