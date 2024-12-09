7045bb1c2abad0acaa8d7c83811eb4a62a555c78
Ersatz-xD <aayan.shazim@gmail.com>
2024-12-09 22:33:20 +0500

    changed package name (AccountDB)

--- src/db/AccountsDb.java
+++ src/db/AccountsDb.java
@@ -5,7 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.regex.*;
-import customExceptions.InvalidInputException;
+import customexceptions.InvalidInputException;
 import model.Account;
 
 public class AccountsDb {
