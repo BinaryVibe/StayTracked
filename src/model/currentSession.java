d69fcc086672dd366effedae9469ab111a6138c6
Ersatz-xD <aayan.shazim@gmail.com>
2024-12-10 15:27:05 +0500

    added more attributes in CurrentSession

--- src/model/currentSession.java
+++ src/model/currentSession.java
@@ -3,6 +3,12 @@ package model;
 
 public class currentSession {
     private static String accountID;
+    private static String userName;
+    private static String firstName;
+    private static String lastName;
+    private static String email;
+    private static String contact;
+    
 
     public static String getAccountID() {
         return accountID;
