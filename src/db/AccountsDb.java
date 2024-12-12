b6ab2818cd8d9c9f841b538d44605f1572b14a38
Ersatz-xD <aayan.shazim@gmail.com>
2024-12-12 21:16:01 +0500

    made connection static

--- src/db/AccountsDb.java
+++ src/db/AccountsDb.java
@@ -10,25 +10,25 @@ import model.Account;
 
 public class AccountsDb {
 
+    static Connection con = DBConnectionManager.getConnection();
+
     //method to validate Input
     public static void validateUser(String email, String password) throws InvalidInputException {
-        try (Connection c = DBConnectionManager.getConnection()) {
-            if (c == null) {
-                throw new InvalidInputException("Database connection failed!");
-            }
+        if (con == null) {
+            throw new InvalidInputException("Database connection failed!");
+        }
 
-            String query = "SELECT password FROM accounts WHERE email = ?";
-            try (PreparedStatement ps = c.prepareStatement(query)) {
-                ps.setString(1, email);
-                try (ResultSet rs = ps.executeQuery()) {
-                    if (rs.next()) {
-                        String passDb = rs.getString("password");
-                        if (!passDb.equals(password)) {
-                            throw new InvalidInputException("Incorrect email or password!");
-                        }
-                    } else {
-                        throw new InvalidInputException("Email not found!");
+        String query = "SELECT password FROM accounts WHERE email = ?";
+        try (PreparedStatement ps = con.prepareStatement(query)) {
+            ps.setString(1, email);
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    String passDb = rs.getString("password");
+                    if (!passDb.equals(password)) {
+                        throw new InvalidInputException("Incorrect email or password!");
                     }
+                } else {
+                    throw new InvalidInputException("Email not found!");
                 }
             }
         } catch (SQLException e) {
@@ -38,67 +38,58 @@ public class AccountsDb {
 
     //method to add a new account in database
     public static void addAccount(Account account) {
-        try(Connection c = DBConnectionManager.getConnection()) {
-            if (c == null) {
-                String error = "Database connection failed!";
-            }
-            
-            //add a logic to insert Account Details in database
-
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
+        if (con == null) {
+            String error = "Database connection failed!";
         }
+
+        //add a logic to insert Account Details in database
     }
-    
+
     public static void validateUserName(String userName) throws InvalidInputException {
-        try(Connection c = DBConnectionManager.getConnection()) {
-            if (c == null) {
-                throw new InvalidInputException("Database connection failed!");
-            }
-            
-            if (userName.isEmpty()){
-                throw new InvalidInputException("Enter user name!");
-            }
-            
-            //add a logic to check if userName is already taken
 
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
+        if (con == null) {
+            throw new InvalidInputException("Database connection failed!");
         }
+
+        if (userName.isEmpty()) {
+            throw new InvalidInputException("Enter user name!");
+        }
+
+        //add a logic to check if userName is already taken
     }
-    
+
     public static void validatePassword(String password) throws InvalidInputException {
-        
+
         // Regex to check valid password.
         String regex = "^(?=.*[0-9])"
-                       + "(?=.*[a-z])(?=.*[A-Z])"
-                       + "(?=.*[@#$%^&+=])"
-                       + "(?=\\S+$).{8,20}$";
+                + "(?=.*[a-z])(?=.*[A-Z])"
+                + "(?=.*[@#$%^&+=])"
+                + "(?=\\S+$).{8,20}$";
         Pattern p = Pattern.compile(regex);
         Matcher m = p.matcher(password);
         boolean validPass = m.matches();
-        
-        if(password.isEmpty()){
+
+        if (password.isEmpty()) {
             throw new InvalidInputException("Enter password!");
         }
-        if(!validPass){
-             throw new InvalidInputException("Password is not strong enough! ");
+        if (!validPass) {
+            throw new InvalidInputException("Password is not strong enough! ");
         }
-        
+
     }
-    
-    public static void validateEmail (String email) throws InvalidInputException{
+
+    public static void validateEmail(String email) throws InvalidInputException {
         // Regex to check valid email
         String regex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]+$";
         Pattern p = Pattern.compile(regex);
         Matcher m = p.matcher(email);
         boolean validEmail = m.matches();
-        
-        if(email.isEmpty()){
+
+        if (email.isEmpty()) {
             throw new InvalidInputException("Enter Email!");
         }
-        if(!validEmail){
-             throw new InvalidInputException("Email s not valid! ");
+        if (!validEmail) {
+            throw new InvalidInputException("Email s not valid! ");
         }
 
     }
