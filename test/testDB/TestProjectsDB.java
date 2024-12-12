7110a0129ab3acaf729092d18565be77ac5d5413
Haris Ali <malikharis2003@gmail.com>
2024-12-12 23:26:49 +0500

    Initial commit

---
+++ test/testDB/TestProjectsDB.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package testDB;
+
+import db.DBConnectionManager;
+
+/**
+ *
+ * @author samtheradiant
+ */
+public class TestProjectsDB {
+    private static conn = DBConnectionManager.con;
+    
+    
+}
