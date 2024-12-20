8922bffde9e793113889a4f515362fb031c65331
Ersatz-xD <aayan.shazim@gmail.com>
2024-12-20 22:18:40 +0500

    update url of db connection

--- src/db/DBConnectionManager.java
+++ src/db/DBConnectionManager.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 
 public class DBConnectionManager {
     
-    static String url = "jdbc:mariadb://192.168.1.14:3306/StayTracked";
+    static String url = "jdbc:mariadb://192.168.1.9:3306/StayTracked";
     static String userName = "Ayaan";
     static String passWord = "123Password";
     static Connection con;
