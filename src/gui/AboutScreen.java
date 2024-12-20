aab2ccef1f5be616d57641aec1e1e00b8c1affd4
Ersatz-xD <aayan.shazim@gmail.com>
2024-12-20 20:38:16 +0500

    initial commit for AboutScreen

---
+++ src/gui/AboutScreen.java
@@ -0,0 +1,27 @@
+
+package gui;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class AboutScreen extends JFrame{
+    public AboutScreen(){
+        setLayout(new BorderLayout());
+        
+        //Setting Upper Panels 
+        //JPanel upperPanel = new JPanel();
+        
+       
+        
+        
+    }
+}
+
+class main {
+    public static void main(String[] args) {
+        AboutScreen frameAB = new AboutScreen();
+        frameAB.setVisible(true);
+    }
+}
