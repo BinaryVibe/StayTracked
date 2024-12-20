2cf2c22749571cb05b03e3f354ea0dbc4894bc1d
Ersatz-xD <aayan.shazim@gmail.com>
2024-12-20 21:40:37 +0500

    basic structure of about us screen

--- src/gui/AboutScreen.java
+++ src/gui/AboutScreen.java
@@ -3,17 +3,47 @@ package gui;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class AboutScreen extends JFrame{
     public AboutScreen(){
-        setLayout(new BorderLayout());
+        //frame properties
+        setLayout(new BorderLayout(5,5));
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(680, 800);
+        setResizable(false);
+        setTitle("About Us");
         
-        //Setting Upper Panels 
-        //JPanel upperPanel = new JPanel();
+        //Add image to be shown
+        ImageIcon myImage = new ImageIcon(getClass().getResource("/icons/pgmanage.jpg"));
         
-       
+       //Setting Upper Panels 
+        JPanel upperPanel = new JPanel();
+        upperPanel.setLayout(new BorderLayout());
+        upperPanel.setPreferredSize(new Dimension(680, 340));
+        upperPanel.setBackground(new Color(21, 25, 34));
+        upperPanel.add(new JLabel(myImage));
+        
+        
+        //Setting Lower Panel
+        JPanel lowerPanel = new JPanel(new BorderLayout());
+        lowerPanel.setBackground(new Color(21, 25, 34));
+        lowerPanel.setPreferredSize(new Dimension(680, 400));
+        JLabel lblAboutUs = new JLabel("ABOUT US");
+        lblAboutUs.setFont(new Font("Segoe UI", 1, 34));
+        lblAboutUs.setForeground(new Color(45,168,216));
+        lblAboutUs.setHorizontalAlignment(JLabel.CENTER);
+        lowerPanel.add(lblAboutUs,BorderLayout.NORTH);
+        
+        
+       //add panels
+       add(upperPanel, BorderLayout.NORTH);
+       add(lowerPanel, BorderLayout.CENTER);
         
         
     }
@@ -22,6 +52,8 @@ public class AboutScreen extends JFrame{
 class main {
     public static void main(String[] args) {
         AboutScreen frameAB = new AboutScreen();
+        frameAB.pack();
+        frameAB.setLocationRelativeTo(null);
         frameAB.setVisible(true);
     }
 }
