216ba7e7949be99955a7bd903a208202578b1e6e
Ersatz-xD <aayan.shazim@gmail.com>
2024-12-21 20:00:27 +0500

    made connection for talal's laptop

--- src/db/DBConnectionManager.java
+++ src/db/DBConnectionManager.java
@@ -7,11 +7,18 @@ import java.sql.SQLException;
 
 public class DBConnectionManager {
     
+    //Connection for hostel internet host:haris
+    /*
     static String url = "jdbc:mariadb://192.168.1.9:3306/StayTracked";
     static String userName = "Ayaan";
     static String passWord = "123Password";
-    static Connection con;
+    static Connection con; */
     
+    //Connection details for Talal
+    static String url = "jdbc:mysql://localhost:3306/StayTracked";
+    static String userName = "root";;
+    static String passWord = "bythebandit@028";
+    static Connection con;
     
     public static void setConnection(){
         con = null;
