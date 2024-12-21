abbe92f86f2442800adb116af436977a904017af
Haris Ali <malikharis2003@gmail.com>
2024-12-21 19:16:12 +0500

    Initial commit

--- src/db/TasksDB.java
+++ src/db/TasksDB.java
@@ -4,11 +4,60 @@
  */
 package db;
 
+import customexceptions.FailureException;
+import helper.CurrentSession;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Connection;
+import model.Task;
+
 /**
  *
  * @author samtheradiant
  */
 public class TasksDB {
 
+    // ATTRIBUTES
     
+    private static final Connection conn = DBConnectionManager.con;
+    
+    // Queries
+    private static final String insertTask = "INSERT INTO tasks (description, start_date, end_date, status, priority, project_id) VALUES (?, ?, ?, ?, ?, ?)";
+
+    // METHODS
+    public static void save(Task task) throws FailureException {
+        try (PreparedStatement saveStmnt = conn.prepareStatement(insertTask, Statement.RETURN_GENERATED_KEYS)) {
+            conn.setAutoCommit(false);
+            saveStmnt.setString(1, task.getDescription());
+            saveStmnt.setDate(2, Date.valueOf(task.getStartDate()));
+            saveStmnt.setDate(3, Date.valueOf(task.getEndDate()));
+            saveStmnt.setString(4, task.getTaskStatus());
+            saveStmnt.setString(5, task.getTaskPriority());
+            saveStmnt.setInt(6, task.getProjectID());
+
+            int affectedRows = saveStmnt.executeUpdate();
+            // int projectId = 0;
+            if (affectedRows > 0) {
+                // pass for now
+            } else {
+                conn.rollback();
+                throw new FailureException("Insert did not work in projects table!");
+            }
+            
+            conn.commit();
+        } catch (SQLException sqle) {
+            System.err.print(sqle.getMessage());
+            if (conn != null) {
+                try {
+                    System.err.print("Transaction is being rolled back");
+                    conn.rollback();
+                } catch (SQLException sqlexcep) {
+                    System.err.print(sqlexcep.getMessage());
+                }
+            }
+        }
+    }
 }
