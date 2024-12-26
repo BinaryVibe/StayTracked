381662bf579beb8057c8a710580e699bfdaf73ee
Haris Ali <malikharis2003@gmail.com>
2024-12-26 10:29:07 +0500

    Change look and feel

--- src/gui/LoginAndSignUp.java
+++ src/gui/LoginAndSignUp.java
@@ -1,5 +1,6 @@
 package gui;
 
+import com.formdev.flatlaf.FlatDarculaLaf;
 import com.formdev.flatlaf.FlatLightLaf;
 import javax.swing.UIManager;
 import db.DBConnectionManager;
@@ -8,7 +9,7 @@ public class LoginAndSignUp {
 
     public static void main(String[] args) {
         try {
-            UIManager.setLookAndFeel(new FlatLightLaf());
+            UIManager.setLookAndFeel(new FlatDarculaLaf());
         } catch (Exception ex) {
             System.err.println("Failed to initialize LaF");
         }
