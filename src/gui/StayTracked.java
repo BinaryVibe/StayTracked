a4ec9969b48b40feb0994dbebabffaac66bf0997
Ersatz-xD <aayan.shazim@gmail.com>
2024-12-26 20:28:09 +0500

    rename main class

---
+++ src/gui/StayTracked.java
@@ -0,0 +1,24 @@
+package gui;
+
+import com.formdev.flatlaf.FlatLightLaf;
+import javax.swing.UIManager;
+import db.DBConnectionManager;
+
+public class StayTracked {
+
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(new FlatLightLaf());
+        } catch (Exception ex) {
+            System.err.println("Failed to initialize LaF");
+        }
+        Login LoginFrame = new Login();
+        
+        LoginFrame.pack();
+        LoginFrame.setLocationRelativeTo(null);
+        LoginFrame.setVisible(true);
+        
+        DBConnectionManager.setConnection();
+    }
+
+}
