83ca81537b457c460cd312e385105c7f16faedb0
Haris Ali <malikharis2003@gmail.com>
2024-12-29 18:52:39 +0500

    Move permission handling here

--- src/db/DatabaseUtils.java
+++ src/db/DatabaseUtils.java
@@ -12,21 +12,23 @@ import java.sql.Date;
 import java.sql.ResultSet;
 import java.time.LocalDate;
 import java.util.ArrayList;
+
 /**
  *
  * @author samtheradiant
  */
 public class DatabaseUtils {
-    
+
     protected static Connection conn = DBConnectionManager.getConnection();
-    
+    private static final String getAccountType = "SELECT account_type FROM accounts WHERE account_id IN (SELECT account_id FROM assigned_to WHERE project_id = ?) AND account_type = 'Manager'";
+
     /**
-     * 
+     *
      * @param query The DML query to update a value
      * @param targetID The primary key of the database entity in which the Enum is in
      * @param newValue The new Enum value
      * @param enumName The name of the Enum e.g, priority, task
-     * @throws SQLException 
+     * @throws SQLException
      */
     public static void updateEnumValue(String query, int targetID, Enum newValue, String enumName) throws SQLException {
         try (PreparedStatement ps = conn.prepareStatement(query)) {
@@ -49,14 +51,14 @@ public class DatabaseUtils {
             }
         }
     }
-    
+
     /**
-     * 
+     *
      * @param query The DML query to update a value
      * @param targetID The primary key of the database entity in which the String is in
      * @param newValue The new String value
      * @param stringName The name of the String e.g, title, description
-     * @throws SQLException 
+     * @throws SQLException
      */
     public static void updateStringValue(String query, int targetID, String newValue, String stringName) throws SQLException {
         try (PreparedStatement ps = conn.prepareStatement(query)) {
@@ -79,14 +81,14 @@ public class DatabaseUtils {
             }
         }
     }
-    
+
     /**
-     * 
+     *
      * @param query The DML query to update a value
      * @param targetID The primary key of the database entity in which the Date is in
      * @param newValue The new Date value
      * @param dateName The name of the Date e.g, start, end, deadline
-     * @throws SQLException 
+     * @throws SQLException
      */
     public static void updateDateValue(String query, int targetID, LocalDate newValue, String dateName) throws SQLException {
         try (PreparedStatement ps = conn.prepareStatement(query)) {
@@ -109,7 +111,7 @@ public class DatabaseUtils {
             }
         }
     }
-    
+
     public static int getWorkCompletion(String query) throws SQLException {
         if (conn == null) {
             throw new SQLException("Database connection is null");
@@ -129,7 +131,7 @@ public class DatabaseUtils {
         percentage = (done / total) * 100;
         return (int) percentage;
     }
-    
+
     public static void deleteRows(String query, ArrayList<Integer> ids) throws SQLException {
         for (int id : ids) {
             try (PreparedStatement deleteStmnt = conn.prepareStatement(query)) {
@@ -148,5 +150,17 @@ public class DatabaseUtils {
                 }
             }
         }
+    }
+
+    public static boolean checkPermission(int projectID) throws SQLException {
+        try (PreparedStatement checkStmnt = conn.prepareStatement(getAccountType)) {
+            checkStmnt.setInt(1, projectID);
+            try (ResultSet result = checkStmnt.executeQuery()) {
+                if (!(result.next())) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
