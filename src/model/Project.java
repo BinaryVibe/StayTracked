1a8a9769067ce3a0b49c474ebde43541d783c8b2
Haris Ali <malikharis2003@gmail.com>
2024-12-30 18:46:18 +0500

    Add javadoc

--- src/model/Project.java
+++ src/model/Project.java
@@ -8,17 +8,32 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 
 /**
+ * Represents a project in the app.
+ * While tasks are related to a project, this class
+ * does not hold any variable to store them.
  *
  * @author samtheradiant
  */
 public class Project extends WorkItem {
 
     // ATTRIBUTES
-    // TODO: Decide whether assignedTo should be added
     private String title;
+    // Holds ids of team members if project is assigned to anyone at creation.
+    // These ids are primary keys of team members in the database
     private ArrayList<Integer> assignedTo;
 
     // METHODS
+    /**
+     * T
+     * 
+     * @param title The title of the project
+     * @param desc The description of the project
+     * @param projectStatus The completion status of the project
+     * @param startDate The start date of the project
+     * @param endDate The end date of the project
+     * @param projectPriority The priority level of the project
+     * @param assignedTo The ids, which were taken from database, of team members 
+     */
     public Project(String title, String desc, Status projectStatus, LocalDate startDate, LocalDate endDate, Priority projectPriority, ArrayList<Integer> assignedTo) {
         super(desc, projectStatus, startDate, endDate, projectPriority);
         this.title = title;
