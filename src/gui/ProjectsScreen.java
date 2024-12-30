65db7220cf20f838ea78b3ff9705932c9fc69cae
Haris Ali <malikharis2003@gmail.com>
2024-12-30 23:31:52 +0500

    Remove useless imports

--- src/gui/ProjectsScreen.java
+++ src/gui/ProjectsScreen.java
@@ -4,9 +4,7 @@
  */
 package gui;
 
-import com.formdev.flatlaf.FlatDarculaLaf;
 import com.formdev.flatlaf.themes.FlatMacDarkLaf;
-import customexceptions.FailureException;
 import db.DBConnectionManager;
 import db.DatabaseUtils;
 import db.ProjectsDB;
